a1a5e8090311c9cace89b2fd84778bc1597f76a9
Matheus Alves <mtsmotog7@gmail.com>
2024-01-15 20:45:33 -0300

    first commit

---
+++ jask/src/main/java/com/jask/jask/JaskApplication.java
@@ -0,0 +1,13 @@
+package com.jask.jask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaskApplication.class, args);
+	}
+
+}
