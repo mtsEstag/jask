76d233600a8673deae827b25a99dd63b5a16d26e
mtsEstag <mtsmotog7@gmail.com>
2024-01-16 16:58:18 -0300

    adicao do data.sql e alteracao do application.properties

---
+++ jask/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- insert into task (title,) values ('teste');
+
+-- insert into task (title, description, creationDate, status) values ('Tarefa 1', 'Descrição da Tarefa 1', '2024-01-16', 'PENDING');
+
+insert into task (title, description, status) values ('First', 'Tarefa 1', 1);
+insert into task (title, description, status) values ('second', 'Tarefa 2', 1);
+insert into task (title, description, status) values ('third', 'Tarefa 3', 1);
+insert into task (title, description, status) values ('fourth', 'Tarefa 4', 1);
+
+insert into task (title, description, status) values ('Fifth', 'Tarefa 5', 1);
+insert into task (title, description, status) values ('Sixth', 'Tarefa 6', 1);
+insert into task (title, description, status) values ('Seventh', 'Tarefa 7', 1);
+insert into task (title, description, status) values ('Eighth', 'Tarefa 8', 1);
